883a3308be32c36416d31e7e09f75cee11a0acf1
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2022-08-10 20:14:22 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Seyit Murat Kaya on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
